36b911171d38719d31cf3b8dc811f0d56158a50e
Balajiw920 <wbalaji2021@gmail.com>
2023-05-22 23:32:25 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* Your other application content */}
+    </div>
+  );
+}
+
+export default App;
